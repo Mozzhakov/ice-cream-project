467c54ff5a477f424e91c1ac5e7af94d700bf210
Mozzhakov <109077027+Mozzhakov@users.noreply.github.com>
2022-09-16 12:37:39 +0000

    Deploying to gh-pages from @ Mozzhakov/ice-cream-project@b2fecb81ae746ffb85400a84593833995cacffa2 🚀

--- index.5f52f966.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.5f52f966.js.map
